cce2fedd5b73cc5c5005cd00f51bdb10e016bd14
André Silva <andre.silva@caelum.com.br>
2012-03-02 21:35:48 -0300

    Subindo projeto para o git

---
+++ src/br/com/caelum/styles/ApplicationStylesActivity.java
@@ -0,0 +1,15 @@
+package br.com.caelum.styles;
+
+import br.com.caelum.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ApplicationStylesActivity extends Activity {
+    
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }    
+}
